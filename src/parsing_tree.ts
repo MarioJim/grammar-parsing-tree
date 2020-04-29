805ae87c51db4bf9ca70ad30fde8a5d28bb5869d
MarioJim <mario.emilio.j@gmail.com>
2020-04-28 22:41:45 -0500

    Changed tree to dendrogram

--- src/parsing_tree.ts
+++ src/parsing_tree.ts
@@ -3,6 +3,31 @@ import { Symbol } from './types';
 
 const width = 600, height = 500, margin = 50;
 
+/**
+ * Checks if a symbol its terminal
+ * @param symbolName the name of the symbol
+ */
+const isTerminalSymbol = (symbolName: string): boolean =>
+  window.grammarStructure.terminalSymbols.includes(symbolName);
+
+/**
+ * Creates a path between point A and point B
+ * @param pointA starting point of the path
+ * @param pointB ending point of the path
+ */
+const generateLinkPath = (
+  pointA: d3.HierarchyPointNode<Symbol>,
+  pointB: d3.HierarchyPointNode<Symbol>
+): string => `
+    M${pointA.x},${height - pointA.y}
+    C${pointA.x},${height - (pointA.y + pointB.y) / 2}
+     ${pointB.x},${height - (pointA.y + pointB.y) / 2}
+     ${pointB.x},${height - pointB.y}`;
+
+/**
+ * Generates the svg structure of a tree on a svg element with id
+ * svgPane on a element with id page
+ */
 export const setupTree = () => {
   // Clear title
   const page = d3.select('#page');
@@ -21,9 +46,9 @@ export const setupTree = () => {
   svg.selectAll('*').remove();
 
   // Initial setup and tree layout
-  const treemap = d3.tree<Symbol>().size([width, height]);
+  const cluster = d3.cluster<Symbol>().size([width, height]);
   const nodesStructure = d3.hierarchy(window.parsingTree, d => d.children);
-  const nodes = treemap(nodesStructure);
+  const nodes = cluster(nodesStructure);
 
   // Add the link between the nodes
   const link = svg.selectAll('.link')
@@ -34,24 +59,25 @@ export const setupTree = () => {
     .attr('fill', 'none')
     .attr('stroke', '#ccc')
     .attr('stroke-width', '2px')
-    .attr('d', d => `M${d.x},${d.y} C${d.x},${(d.y + d.parent.y) / 2} ${d.parent.x},${(d.y + d.parent.y) / 2} ${d.parent.x},${d.parent.y}`);
+    .attr('d', d => generateLinkPath(d, d.parent));
 
   // Add the nodes as circles
   const node = svg.selectAll('.node')
     .data(nodes.descendants())
     .enter()
     .append('g')
-    .attr('transform', d => `translate(${d.x},${d.y})`);
+    .attr('transform', d => `translate(${d.x},${height - d.y})`);
 
   // Creates a circle where the node is supposed to be
   node.append('circle')
     .attr('r', 18)
-    .attr('fill', 'deepskyblue');
+    .attr('fill', 'white')
+    .attr('stroke-width', 3)
+    .attr('stroke', d => isTerminalSymbol(d.data.name) ? 'steelblue' : 'green');
 
   // Adds the symbol name to the node
   node.append('text')
     .attr('dy', 5)
     .style('text-anchor', 'middle')
     .text(d => d.data.name);
-
 };
